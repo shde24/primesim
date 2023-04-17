6002ddc813e42adfb836c02f1e18f32b1cbc1a7b
WavetecRR // Thomas Hall <126712774+wavetecrr@users.noreply.github.com>
2023-04-17 10:23:48 +0100

    Add files via upload

---
+++ Mods/Stepford Mod/Play Prime Simulator 2023 Stepford Mod!.py
@@ -0,0 +1,22 @@
+import time
+print("Welcome to Prime Simulator 2023 Stepford Mod!")
+shop = input("Where will you shop?")
+if shop == "Willowfield":
+    print("OUT OF STOCK!!!!! MISSION FAILED :(")
+if shop == "Woodhead Lane":
+    print("YOU DIDN'T PRAISE CHARLIE CHRIST!!!! MISIION FAILED :(")
+if shop == "Upper Staploe":
+    print("YOU WAITED ALL DAY FOR A TRAIN AND YOU DIDN'T GET TO THE DAMN STORE. MISSION FAILED :(")
+if shop == "Avatar Shop":
+    print("YOU BOUGHT FAKE PRIME! MISSION FAILED :(")
+if shop == "RoWeb":
+    print("THIS IS BLOXAIDE! SOMEONE REFILLED THE BOTTLE! MISIION FAILED :(")
+if shop == "Sahara":
+    print("YOU FINALLY GOT SOME PRIME! MISSION PASSED :D")
+
+print("If you want to play again you need to restart the script lol")
+time.sleep(60)
+
+
+
+
---
+++ Play Prime Simulator 2023!.py
@@ -0,0 +1,22 @@
+import time
+print("Welcome to Prime Simulator 2023!")
+shop = input("Where will you shop?")
+if shop == "Asda":
+    print("OUT OF STOCK!!!!! MISSION FAILED :(")
+if shop == "Wakefield Wines":
+    print("IN STOCK FOR £2.5K?! MISIION FAILED :(")
+if shop == "Aldi":
+    print("YOU WAITED ALL DAY FOR THE LINE TO GO DOWN AND YOU DIDN'T EVEN ENTER THE DAMN STORE. MISSION FAILED :(")
+if shop == "AliExpress":
+    print("YOU BOUGHT FAKE PRIME! MISSION FAILED :(")
+if shop == "eBay":
+    print("THIS IS POWERADE! SOMEONE REFILLED THE BOTTLE! MISIION FAILED :(")
+if shop == "Amazon":
+    print("YOU FINALLY GOT SOME PRIME! MISSION PASSED :D")
+
+print("If you want to play again you need to restart the script lol")
+time.sleep(60)
+
+
+
+
